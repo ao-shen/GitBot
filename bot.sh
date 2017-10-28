e4de6a39f8e27355b44c32799e123046f4228440
ao-shen <ao.shen@gmail.com>
2017-10-27 20:47:56 -0400

    made changes

--- bot.sh
+++ bot.sh
@@ -5,7 +5,7 @@
 #echo Here is my first line
 #echo Here is my second line
 #echo Here is my third line
-@echo Updated: %date% %time%
+echo Updated: %date% %time%
 )>"txt.txt"
 git add .
 git commit -am "made changes"
