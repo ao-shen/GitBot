090f06fba8c567355ae7d1c6484e3d0812ba1a28
ao-shen <ao.shen@gmail.com>
2017-10-27 20:51:33 -0400

    made changes

--- bot.sh
+++ bot.sh
@@ -1,14 +1,23 @@
 #!/bin/sh
 
 #git checkout dev
-(
+
 #echo Here is my first line
 #echo Here is my second line
 #echo Here is my third line
-echo Updated: %date% %time%
-)>"txt.txt"
-git add .
-git commit -am "made changes"
-git push
-#echo Press Enter...
-#read
+while [ : ]
+do
+    #clear
+    # Get the system time
+	(
+		now="$(date +"%r")"
+		echo "$now"
+	)>"txt.txt"
+	git add .
+	git commit -am "made changes"
+	git push
+	#echo Press Enter...
+	#read
+	timeout 5
+done
+
